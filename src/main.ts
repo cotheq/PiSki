480ea34d875467c7ae29fa7bd59b80c351ab552b
Andrew Kot <rgled@ya.ru>
2024-12-05 22:11:24 +0700

    add pixi container

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { initPixiApplication } from './pixiContainer';
+
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div>
-  Test
-</div>
+const mainElement = document.querySelector<HTMLDivElement>('#app')!;
+const pixiViewID = "pixiView";
 
+
+const observer = new MutationObserver((mutationsList, observer) => {
+  //console.log(mutationsList);
+  const pixiViewElement = document.querySelector(`#${pixiViewID}`) as HTMLCanvasElement;
+  if (!pixiViewElement) {
+    throw new DOMException("No view element for PIXI")
+  }
+  initPixiApplication(pixiViewElement).then(() => console.log("PIXI view initialized"));
+});
+
+observer.observe(mainElement, { characterData: false, childList: true, attributes: false });
+
+mainElement.innerHTML = `
+<div id="test">
+  <canvas id="${pixiViewID}"></canvas>
+</div>
 `
